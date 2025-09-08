09d5b61dbb5c2be2af9454920ac7213dbbc7df50
tcyh035 <jsyh_1123@163.com>
2025-09-08 13:30:29 +0800

    fix write image error

--- src/sphere.hpp
+++ src/sphere.hpp
@@ -13,7 +13,47 @@ public:
     float Kd, Ks;
 
     float specularExponent;
-};
 
+    
+    static bool solveQuadratic(const float& a, const float& b, const float& c, float& x0, float& x1)
+    {
+        float discr = b * b - 4 * a * c;
+        if (discr < 0)
+            return false;
+        else if (discr == 0)
+            x0 = x1 = -0.5 * b / a;
+        else
+        {
+            float q = (b > 0) ? -0.5 * (b + sqrt(discr)) : -0.5 * (b - sqrt(discr));
+            x0 = q / a;
+            x1 = c / q;
+        }
+        if (x0 > x1)
+            std::swap(x0, x1);
+        return true;
+    }
+
+    bool intersect(const Eigen::Vector3f& orig, const Eigen::Vector3f& dir, float& tnear) const
+    {
+        Eigen::Vector3f L = orig - center;
+        float a = dir.dot(dir);
+        float b = 2 * dir.dot(L);
+        float c = L.dot(L) - radius * radius;
+        float t0, t1;
+        if (!solveQuadratic(a, b, c, t0, t1))
+            return false;
+        if (t0 < 0)
+            t0 = t1;
+        if (t0 < 0)
+            return false;
+        tnear = t0;
+
+        return true;
+    }
+
+    Eigen::Vector3f getNormal(const Eigen::Vector3f& hitPoint) const {
+        return (hitPoint - center).normalized();
+    }
+};
 
 #endif
--- src/utils.hpp
+++ src/utils.hpp
@@ -15,16 +15,15 @@ public:
         // <width> <height>
         // <max_color_value>
         // <pixel data>
-        FILE* fp = fopen(filename.c_str(), "wb");
+        FILE* fp = fopen(filename.c_str(), "w");
         fwrite("P3\n", 1, 3, fp);
         fprintf(fp, "%d %d\n", width, height);
         fwrite("255\n", 1, 4, fp);
         for (auto i = 0; i < width * height; ++i) {
-            static unsigned char color[3];
-            color[0] = (unsigned char)(255 * std::max(0.f, std::min(1.0f, framebuffer[i].x())));
-            color[1] = (unsigned char)(255 * std::max(0.f, std::min(1.0f, framebuffer[i].y())));
-            color[2] = (unsigned char)(255 * std::max(0.f, std::min(1.0f, framebuffer[i].z())));
-            fwrite(color, 1, 3, fp);
+            int r = (int)(255 * std::max(0.f, std::min(1.0f, framebuffer[i].x())));
+            int g = (int)(255 * std::max(0.f, std::min(1.0f, framebuffer[i].y())));
+            int b = (int)(255 * std::max(0.f, std::min(1.0f, framebuffer[i].z())));
+            fprintf(fp, "%d %d %d ", r, g, b);
         }
         fclose(fp);
     }
