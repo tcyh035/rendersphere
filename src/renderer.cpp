8f6dec8e250a0366846ae9c432dbf16d2d3c0214
tcyh035 <jsyh_1123@163.com>
2025-09-08 13:30:51 +0800

    test renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -9,8 +9,11 @@
 
 void Renderer::render(const Scene& scene) {
     std::vector<Eigen::Vector3f> framebuffer(scene.width * scene.height);
-    
-    
+    for (int j = 0; j < scene.height; ++j) {
+        for (int i = 0; i < scene.width; ++i) {
+            framebuffer[i + j * scene.width] = Eigen::Vector3f(0.1, 0.2, 0.3); // test background color
+        }
+    }
 
     Utils::WriteImage(framebuffer, scene.width, scene.height, "output.ppm");
     std::cout << "Test" << std::endl;
