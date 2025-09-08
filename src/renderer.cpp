201d110fc2275242a913922994fa54cd00c00171
tcyh035 <jsyh_1123@163.com>
2025-09-08 13:15:28 +0800

    write utils to render image

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,7 +1,17 @@
 #include "renderer.h"
 
+#include "scene.hpp"
+#include "utils.hpp"
+
+#include <Eigen/Core>
+
 #include <iostream>
 
-void Renderer::render(const Scene & scene) {
+void Renderer::render(const Scene& scene) {
+    std::vector<Eigen::Vector3f> framebuffer(scene.width * scene.height);
+    
+    
+
+    Utils::WriteImage(framebuffer, scene.width, scene.height, "output.ppm");
     std::cout << "Test" << std::endl;
 }
